330e1f4142c004625c07da76e71d4e618655f503
taek105 <ht5172@gmail.com>
2025-06-05 07:35:33 +0000

    fix(product_post_gen.py): 반환 날짜 한국 시간 기준으로 수정

--- v1/app/node/product_post_gen.py
+++ v1/app/node/product_post_gen.py
@@ -3,6 +3,7 @@ from llm.factory import get_desc_gen_client, get_title_gen_client
 from config import PRODUCT_DESC_GEN_PROMPT, PRODUCT_TITLE_GEN_PROMPT, node_log
 from typing import Dict
 from datetime import datetime, timedelta
+from zoneinfo import ZoneInfo
 
 import asyncio
 
@@ -51,7 +52,7 @@ async def generate_title(state: Dict) -> str:
 # async def generate_date(state: Dict) -> str:
 
 def generate_pickup_date():
-    now = datetime.now()
+    now = datetime.now(ZoneInfo("Asia/Seoul"))
     floored_minute = (now.minute // 30) * 30
     floored = now.replace(minute=floored_minute, second=0, microsecond=0)
     due_datetime = floored + timedelta(days=9)
@@ -59,7 +60,7 @@ def generate_pickup_date():
     return due_datetime.strftime("%Y-%m-%dT%H:%M")
 
 def generate_due_date():
-    now = datetime.now()
+    now = datetime.now(ZoneInfo("Asia/Seoul"))
     floored_minute = (now.minute // 30) * 30
     floored = now.replace(minute=floored_minute, second=0, microsecond=0)
     due_datetime = floored + timedelta(days=7)
