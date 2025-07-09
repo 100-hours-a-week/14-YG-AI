c51199270512be07ba8211dae3fcc90057c9c642
taek105 <ht5172@gmail.com>
2025-07-09 07:58:20 +0000

    fix(crawl_thumbnail): 컨테이너 환경에서 크롬드라이버가 제대로 동작하지 않는 이슈
    - options를 추가했습니다.

--- v1/app/node/tool/crawl_thumbnail.py
+++ v1/app/node/tool/crawl_thumbnail.py
@@ -103,6 +103,10 @@ def capture_thumbnail(url, output_path = 'img/thumbnail.jpg'):
 
     options = Options()
     options.add_argument('--headless')
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+
     options.add_experimental_option(
         "mobileEmulation",
         {
